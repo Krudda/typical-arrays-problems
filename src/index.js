46b288c6de4e74f0599215e2a3516af8bd7a5886
Dmitriy <krudda@yandex.ru>
2020-02-26 00:42:06 +0300

    fead: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!Array.isArray(array) || Object.keys(array).length == 0) return 0;
+
+    let minimum = array.reduce(function(min, current){
+        return current < min ? current : min;
+    }, Infinity);
+    return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || Object.keys(array).length == 0) return 0;
+
+    let maximum = array.reduce(function(max, current){
+        return current > max ? current : max;
+    }, -Infinity);
+    return maximum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || Object.keys(array).length == 0) return 0;
+    
+    let summ = array.reduce(function(sum, current){
+        return sum+current;
+    }, 0);
+  return summ/(Object.keys(array).length);
 }
